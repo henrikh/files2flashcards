0269d9103010696c956e09dad56e98ab412c7221
Henrik Enggaard Hansen <henrik.enggaard@gmail.com>
2020-05-16 16:18:55 +0200

    Move integration tests to their own repository

--- tests/test_files2flashcards.py
+++ tests/test_files2flashcards.py
@@ -442,48 +442,5 @@ class TestProcessFile(unittest.TestCase):
 
         self.assertEqual(f2f.process_file.call_count, 1)
 
-class TestIntegration(unittest.TestCase):
-
-    def test_new_note(self):
-        f2f.AnkiConnectWrapper.deck_name = "Test"
-        f2f.AnkiConnectWrapper.invoke("createDeck", {"deck": "Test"})
-
-        f2f.add_format(
-            tag="abbr",
-            class_name="h-fcard",
-            note_type="Basic",
-            mapping_function=f2f.extract_abbreviation_basic)
-
-        f2f.add_format(
-            tag="span",
-            class_name="e-cloze",
-            note_type="Cloze",
-            mapping_function=f2f.extract_cloze)
-
-        tmp_dir_o = tempfile.TemporaryDirectory()
-        tmp_dir = tmp_dir_o.name
-        shutil.copyfile("tests/test.tid", tmp_dir + "/" + "test.tid")
-
-        f2f.process_file(tmp_dir + "/" + "test.tid")
-
-        with open(tmp_dir + "/" + "test.tid", "r+", encoding='utf-8') as f:
-            content = f.read()
-
-            print(content)
-
-            content = content.replace("BER", "SER")
-            content = content.replace("Bit", "Symbol")
-            content = content.replace("replace-me", "<em>replace-me</em>")
-
-            f.seek(0)
-            f.write(content)
-            f.seek(0)
-            content = f.read()
-            print(content)
-
-        f2f.process_file(tmp_dir + "/" + "test.tid")
-
-        f2f.AnkiConnectWrapper.invoke("deleteDecks", {"decks": ["Test"], "cardsToo": True})
-
 if __name__ == '__main__':
     unittest.main()
